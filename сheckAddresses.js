19032159f44efcbd68c62f29ee559bd96090b890
p3810cyber <91782946+p3810cyber@users.noreply.github.com>
2023-04-09 12:30:42 +0200

    Update сheckAddresses.js

--- сheckAddresses.js
+++ сheckAddresses.js
@@ -5,11 +5,17 @@ const addresses = JSON.parse(fs.readFileSync('addresses.json'));
 
 const files = ['arbitrum.json', 'bsc.json', 'ethereum.json', 'optimism.json', 'polygon.json'];
 
+find = false;
 for (const file of files) {
   const data = JSON.parse(fs.readFileSync(file));
   for (const address of addresses) {
     if (data.includes(address)) {
+        find = true;
       console.log('\x1b[31m%s\x1b[0m', `${address} found in ${file}`);
     }
   }
+}
+
+if (!find) {
+    console.log("%cВсе хорошо!!!", "color: green");
 }
